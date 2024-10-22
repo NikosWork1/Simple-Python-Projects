7a3cc813bdebc9b1550a67f2b3580f59b7c47e95
Tzanis Mouflouzis <tzmouflou@gmail.com>
2024-10-22 13:11:54 +0300

    Max_number.py

--- Simple_Python_Code/Max_number.py
+++ Simple_Python_Code/Max_number.py
@@ -1,6 +1,6 @@
-numbers=[56,241,6,454,154,1544,784512,3216546341,23121015241,1233123612]
-max_number=numbers[0]
+numbers = [56,241,6,454,154,1544,784512,3216546341,23121015241,1233123612]
+max_number = numbers[0]
 for num in numbers:
-    if num>max_number:
-        max_number=num
+    if num > max_number :
+        max_number = num
 print(f"Max number is {max_number}")
