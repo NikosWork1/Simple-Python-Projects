104347c15cd191d79c0798ca3bdbfb752d2dda09
Tzanis Mouflouzis <tzmouflou@gmail.com>
2024-10-22 13:11:20 +0300

    Filtering_Even_Numbers.py

--- Simple_Python_Code/Filtering_Even_Numbers.py
+++ Simple_Python_Code/Filtering_Even_Numbers.py
@@ -1,6 +1,6 @@
-list1=[55,65,321,85412,842415,65462,64]
-list2=[]
-for num in list1:
+list1 = [55,65,321,85412,842415,65462,64]
+list2 = []
+for num in list1 :
     if num % 2 == 0:
         list2.append(num)
 print("Even numbers in the list",list2)
