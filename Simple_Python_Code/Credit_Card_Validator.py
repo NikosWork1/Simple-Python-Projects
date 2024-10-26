52785cb74dfee19a77ecc0acb38d7980821c5d1f
Tzanis Mouflouzis <tzmouflou@gmail.com>
2024-10-26 11:18:45 +0300

    Add files via upload

---
+++ Simple_Python_Code/Credit_Card_Validator.py
@@ -0,0 +1,24 @@
+sum_odd_digits = 0
+sum_even_digits = 0
+total = 0
+card_number = input("Enter you credit card number: ")
+card_number = card_number.replace("-", "")
+card_number = card_number.replace(" ", "")
+card_number = card_number[::-1]
+
+for x in card_number[::2]:
+    sum_odd_digits += int(x)
+
+for x in card_number[1::2]:
+    x = int(x) * 2
+    if x >= 10 :
+        sum_even_digits += (1 + (x % 10))
+    else:
+        sum_even_digits += x
+
+total = sum_odd_digits + sum_even_digits
+
+if total % 10 == 0:
+    print("Your credit card number is valid")
+else:
+    print("Your credit card number is invalid")
